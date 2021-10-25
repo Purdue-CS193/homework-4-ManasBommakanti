7b7982bfe1a5a68f8e18af4ce70c289b14ba32f7
Manas Bommakanti <mbommaka@data.cs.purdue.edu>
2021-10-25 14:16:38 -0400

    Changed basicCalculator to use if-else

--- src/Test1.java
+++ src/Test1.java
@@ -8,28 +8,19 @@ public class Test1 {
 
     public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
-
-        switch(operation) {
-            case ADD:
-                // if operation = ADD, return the sum
-                result = num1 + num2;
-                break;
-            case SUBTRACT:
-                result = num1 - num2;
-                break;
-            case MULTIPLY:
-                result = num1 * num2;
-                break;
-            case DIVIDE:
-                if (num2 == 0) {
-                    System.out.println("Can't divide by 0");
-                } else {
-                    result = ((double)num1) / num2;
-                }
-                break;
-        }
-        return result;
-    }
+        
+	if (operation == Operation.ADD)
+		return num1 + num2;
+	if (operation == Operation.SUBTRACT)
+		return num1 - num2;
+	if (operation == Operation.MULTIPLY)
+		return num1 * num2;
+	if (num2 == 0) {
+		System.out.println("Can't divide by 0");
+		return 0;
+	}
+	return ((double) num1) / num2;
+   }
 
     public static void main(final String[] args) {
         System.out.println(basicCalculator(Operation.DIVIDE, 1, 2));
